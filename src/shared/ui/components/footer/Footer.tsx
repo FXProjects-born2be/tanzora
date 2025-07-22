64b06a459b1547f0ae97a62b0820220cf4805065
SergWebAcceptance <serg.web@acceptance.ltd>
2025-07-22 18:29:03 +0300

    Refactor footer component to enhance layout and responsiveness; update translations and contact information

--- src/shared/ui/components/footer/Footer.tsx
+++ src/shared/ui/components/footer/Footer.tsx
@@ -7,6 +7,9 @@ import { useTranslations } from 'next-intl';
 
 import styles from './Footer.module.scss';
 
+import { RequestForm } from '@/featured/contact-popup/ui/RequestForm/RequestForm';
+import { ThankyouModal } from '@/featured/contact-popup/ui/ThankyouModal/ThankyouModal';
+
 export const Footer = () => {
   const t = useTranslations('footer');
   const year = new Date().getFullYear();
@@ -14,34 +17,36 @@ export const Footer = () => {
   return (
     <footer className={styles.footer} id="footer">
       <div className={'_container'}>
-        <Link href="/" className={styles.footer__logo}>
-          <Image src="/images/logo.svg" alt="Tanzora" width={212} height={49} />
-        </Link>
         <div className={styles.footer__top}>
           <div className={styles.footer__col}>
-            <p>
-              {t('description', {
-                fallback:
-                  'Tanzora ofrece una plataforma de trading segura e intuitiva, brindando acceso a mercados globales, incluyendo Forex, acciones, materias primas, índices y criptomonedas. Dedicados a la innovación y soporte, empoderamos a los traders para alcanzar sus metas financieras.',
-              })}
-            </p>
-            <p>
-              {year} © {t('copyright', { fallback: 'Tanzora. All Rights Reserved.' })}
-            </p>
+            <Link href="/" className={styles.footer__logo}>
+              <Image src="/images/logo.svg" alt="Tanzora" width={212} height={49} />
+            </Link>
+            <h4>Brandnova Studios LLC</h4>
+            <div className={styles.address}>
+              <div>
+                <Link href="mailto:info@tanzora.io">info@tanzora.io</Link>
+              </div>
+              <div>
+                <Link href="tel:+442036756517">+442036756517</Link>
+              </div>
+              <div>
+                <p>
+                  Desarrollo Hamilton, Unidad B, <br />
+                  Charlestown, Nevis, Indias Occidentales
+                </p>
+              </div>
+            </div>
           </div>
           <div className={styles.footer__col}>
-            <div>
-              <h5>{t('address', { fallback: 'Address' })}</h5>
-              <p>
-                Hamilton Development, Unit B, <br />
-                Charlestown, Nevis, West Indies
-              </p>
-            </div>
-            <div>
-              <h5>{t('number', { fallback: 'Number' })}</h5>
-              <p>No. L 23777</p>
-            </div>
+            <RequestForm />
+            <ThankyouModal />
           </div>
+        </div>
+        <div className={styles.footer__bottom}>
+          <p>
+            {year} © {t('copyright', { fallback: 'Tanzora. All Rights Reserved.' })}
+          </p>
         </div>
       </div>
     </footer>
--- src/shared/ui/kit/button/Button.tsx
+++ src/shared/ui/kit/button/Button.tsx
@@ -6,14 +6,18 @@ import { ButtonArrow } from '@/shared/ui/icons';
 
 import styles from './Button.module.scss';
 
-import { useRequestStore } from '@/featured/contact-popup/store/requestStore';
-
 export const Button = ({ className }: { className?: string }) => {
   const t = useTranslations('button');
-  const { open } = useRequestStore();
+
+  const scrollToFooter = () => {
+    const footer = document.getElementById('footer');
+    if (footer) {
+      footer.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
-    <button className={`${className ? styles[className] : styles.button}`} onClick={open}>
+    <button className={`${className ? styles[className] : styles.button}`} onClick={scrollToFooter}>
       {t('button', {
         fallback: 'Vamos a hablar',
       })}
